4cddecd909c900f19b48d9882bb03c11209ea962
Johnny C. Lam <jlam88@gmail.com>
2022-12-16 18:56:25 -0500

    fix: query the terminal for color support for Spaceship Lite
    Instead of hardcoding a check for `bash`, `ksh`, or `zsh` to
    determine whether we can use color codes (which is clearly wrong
    anyway), do it the right way and query the terminal for color
    support.

--- .config/shell/spaceship-lite/colors.sh
+++ .config/shell/spaceship-lite/colors.sh
@@ -1,24 +1,42 @@
 # spaceship-lite-colors.sh
 
 # Global color variables to be used by Spaceship Lite modules.
-if [ -n "${BASH_VERSION}${KSH_VERSION}${ZSH_VERSION}" ]; then
-	SS_ESC_BLUE='[1;34m'
-	SS_ESC_CYAN='[1;36m'
-	SS_ESC_GRAY='[1;30m'
-	SS_ESC_GREEN='[1;32m'
-	SS_ESC_PURPLE='[1;35m'
-	SS_ESC_RED='[1;31m'
-	SS_ESC_WHITE='[1;37m'
-	SS_ESC_YELLOW='[1;33m'
-	SS_ESC_END='[m'
-else
-	SS_ESC_BLUE=
-	SS_ESC_CYAN=
-	SS_ESC_GRAY=
-	SS_ESC_GREEN=
-	SS_ESC_PURPLE=
-	SS_ESC_RED=
-	SS_ESC_WHITE=
-	SS_ESC_YELLOW=
-	SS_ESC_END=
+SS_ESC_BLUE=
+SS_ESC_CYAN=
+SS_ESC_GRAY=
+SS_ESC_GREEN=
+SS_ESC_PURPLE=
+SS_ESC_RED=
+SS_ESC_WHITE=
+SS_ESC_YELLOW=
+SS_ESC_END=
+
+if [ -t 1 ]; then
+	ss_esc_bold=$(tput bold)
+	if [ -n "${ss_esc_bold}" ]; then
+		ss_ncolors=$(tput colors)
+		if [ -n "${ss_ncolors}" ] && [ ${ss_ncolors} -ge 8 ]; then
+			SS_ESC_BLUE=${ss_esc_bold}$(tput setaf 4)
+			SS_ESC_CYAN=${ss_esc_bold}$(tput setaf 6)
+			SS_ESC_GRAY=${ss_esc_bold}$(tput setaf 0)
+			SS_ESC_GREEN=${ss_esc_bold}$(tput setaf 2)
+			SS_ESC_PURPLE=${ss_esc_bold}$(tput setaf 5)
+			SS_ESC_RED=${ss_esc_bold}$(tput setaf 1)
+			SS_ESC_WHITE=${ss_esc_bold}$(tput setaf 7)
+			SS_ESC_YELLOW=${ss_esc_bold}$(tput setaf 3)
+			SS_ESC_END=$(tput sgr0)
+		else
+			SS_ESC_BLUE=${ss_esc_bold}
+			SS_ESC_CYAN=${ss_esc_bold}
+			SS_ESC_GRAY=${ss_esc_bold}
+			SS_ESC_GREEN=${ss_esc_bold}
+			SS_ESC_PURPLE=${ss_esc_bold}
+			SS_ESC_RED=${ss_esc_bold}
+			SS_ESC_WHITE=${ss_esc_bold}
+			SS_ESC_YELLOW=${ss_esc_bold}
+			SS_ESC_END=$(tput sgr0)
+		fi
+		unset ss_ncolors
+	fi
+	unset ss_esc_bold
 fi
