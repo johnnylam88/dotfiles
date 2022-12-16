2fec81d919ba221baa6091968dd20bbc66ce04aa
Johnny C. Lam <jlam88@gmail.com>
2022-12-15 23:50:36 -0500

    fix: change escape code variables into globals in Spaceship Lite
    Change the `ss_esc_*` color escape code variable definitions into
    global variables so they can be used directly by modules.

--- .config/shell/spaceship-lite/spaceship-lite.sh
+++ .config/shell/spaceship-lite/spaceship-lite.sh
@@ -1,25 +1,26 @@
 # spaceship-lite.sh
 
+# Global color variables to be used by Spaceship Lite modules.
 if [ -n "${BASH_VERSION}${KSH_VERSION}${ZSH_VERSION}" ]; then
-	ss_esc_blue='[1;34m'
-	ss_esc_cyan='[1;36m'
-	ss_esc_gray='[1;30m'
-	ss_esc_green='[1;32m'
-	ss_esc_purple='[1;35m'
-	ss_esc_red='[1;31m'
-	ss_esc_white='[1;37m'
-	ss_esc_yellow='[1;33m'
-	ss_esc_end='[m'
+	SS_ESC_BLUE='[1;34m'
+	SS_ESC_CYAN='[1;36m'
+	SS_ESC_GRAY='[1;30m'
+	SS_ESC_GREEN='[1;32m'
+	SS_ESC_PURPLE='[1;35m'
+	SS_ESC_RED='[1;31m'
+	SS_ESC_WHITE='[1;37m'
+	SS_ESC_YELLOW='[1;33m'
+	SS_ESC_END='[m'
 else
-	ss_esc_blue=
-	ss_esc_cyan=
-	ss_esc_gray=
-	ss_esc_green=
-	ss_esc_purple=
-	ss_esc_red=
-	ss_esc_white=
-	ss_esc_yellow=
-	ss_esc_end=
+	SS_ESC_BLUE=
+	SS_ESC_CYAN=
+	SS_ESC_GRAY=
+	SS_ESC_GREEN=
+	SS_ESC_PURPLE=
+	SS_ESC_RED=
+	SS_ESC_WHITE=
+	SS_ESC_YELLOW=
+	SS_ESC_END=
 fi
 
 ss_git_prompt_sourced=
@@ -78,33 +79,33 @@ PS1="${PS1}"'┌ '
 # Add the user if this is not localhost.
 if [ -n "${SSH_CONNECTION}" ]; then
 	case ${USER} in
-	root)	PS1="${PS1}${ss_esc_red}${ss_prompt_user}${ss_esc_end}" ;;
-	*)		PS1="${PS1}${ss_esc_yellow}${ss_prompt_user}${ss_esc_end}" ;;
+	root)	PS1="${PS1}${SS_ESC_RED}${ss_prompt_user}${SS_ESC_END}" ;;
+	*)		PS1="${PS1}${SS_ESC_YELLOW}${ss_prompt_user}${SS_ESC_END}" ;;
 	esac
 fi
 # Add the last component of the current directory.
 if [ -n "${ss_prompt_pwd}" ]; then
 	if [ -n "${SSH_CONNECTION}" ]; then
-		PS1="${PS1} ${ss_esc_white}in${ss_esc_end} "
+		PS1="${PS1} ${SS_ESC_WHITE}in${SS_ESC_END} "
 	fi
-	PS1="${PS1}${ss_esc_cyan}${ss_prompt_pwd}${ss_esc_end}"
+	PS1="${PS1}${SS_ESC_CYAN}${ss_prompt_pwd}${SS_ESC_END}"
 fi
 # Add the hostname if we're in an SSH session.
 if [ -n "${SSH_CONNECTION}" ]; then
-	PS1="${PS1} ${ss_esc_white}at${ss_esc_end}"
-	PS1="${PS1} ${ss_esc_green}${ss_prompt_host}${ss_esc_end}"
+	PS1="${PS1} ${SS_ESC_WHITE}at${SS_ESC_END}"
+	PS1="${PS1} ${SS_ESC_GREEN}${ss_prompt_host}${SS_ESC_END}"
 fi
 if [ -n "${ss_git_prompt_sourced}" ]; then
 	# Append Git status.
-	ss_git_status_fmt=" ${ss_esc_white}on${ss_esc_end} ${ss_esc_purple} %s${ss_esc_end}"
+	ss_git_status_fmt=" ${SS_ESC_WHITE}on${SS_ESC_END} ${SS_ESC_PURPLE} %s${SS_ESC_END}"
 	PS1="${PS1}"'$(__git_ps1 "${ss_git_status_fmt}")'
 fi
 # Add container name if we're in a container.
 if [ -f /run/.containerenv ]; then
 	ss_container_name=$(. /run/.containerenv && printf ${name})
 	if [ -n "${ss_container_name}" ]; then
-		PS1="${PS1} ${ss_esc_white}on${ss_esc_end}"
-		PS1="${PS1} ${ss_esc_cyan}⬢ (${ss_container_name})${ss_esc_end}"
+		PS1="${PS1} ${SS_ESC_WHITE}on${SS_ESC_END}"
+		PS1="${PS1} ${SS_ESC_CYAN}⬢ (${ss_container_name})${SS_ESC_END}"
 	fi
 	unset ss_container_name
 fi
@@ -116,7 +117,4 @@ PS1="${PS1}"'
 #       editing may break.
 PS1="${PS1}"'└ '
 
-unset ss_esc_blue ss_esc_cyan ss_esc_gray
-unset ss_esc_green ss_esc_purple ss_esc_red
-unset ss_esc_white ss_esc_yellow ss_esc_end
 unset ss_prompt_host ss_prompt_pwd ss_prompt_user
