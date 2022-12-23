28214b3b79a263fd9f8fe539735de5a5ca883af7
Johnny C. Lam <jlam88@gmail.com>
2022-12-22 23:05:00 -0500

    refactor: move `colors` code inside an `init` function
    Move the top-level code in the `colors` module that defines the
    color variables used by other modules into an `init` function.

--- .config/shell/starfighter/colors.sh
+++ .config/shell/starfighter/colors.sh
@@ -11,37 +11,41 @@ STARFIGHTER_WHITE=
 STARFIGHTER_YELLOW=
 STARFIGHTER_NORMAL=
 
-if [ -t 1 ]; then
-	sf_start=${STARFIGHTER_ESC_START}
-	sf_end=${STARFIGHTER_ESC_END}
-	sf_bold=$(tput bold)
-	if [ -n "${sf_bold}" ]; then
-		sf_ncolors=$(tput colors)
-		if [ -n "${sf_ncolors}" ] && [ ${sf_ncolors} -ge 8 ]; then
-			STARFIGHTER_BLUE=${sf_start}${sf_bold}$(tput setaf 4)${sf_end}
-			STARFIGHTER_CYAN=${sf_start}${sf_bold}$(tput setaf 6)${sf_end}
-			STARFIGHTER_GRAY=${sf_start}${sf_bold}$(tput setaf 0)${sf_end}
-			STARFIGHTER_GREEN=${sf_start}${sf_bold}$(tput setaf 2)${sf_end}
-			STARFIGHTER_MAGENTA=${sf_start}${sf_bold}$(tput setaf 5)${sf_end}
-			STARFIGHTER_RED=${sf_start}${sf_bold}$(tput setaf 1)${sf_end}
-			STARFIGHTER_WHITE=${sf_start}${sf_bold}$(tput setaf 7)${sf_end}
-			STARFIGHTER_YELLOW=${sf_start}${sf_bold}$(tput setaf 3)${sf_end}
-			STARFIGHTER_NORMAL=${sf_start}$(tput sgr0)${sf_end}
-		else
-			STARFIGHTER_BLUE=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_CYAN=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_GRAY=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_GREEN=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_MAGENTA=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_RED=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_WHITE=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_YELLOW=${sf_start}${sf_bold}${sf_end}
-			STARFIGHTER_NORMAL=${sf_start}$(tput sgr0)${sf_end}
+starfighter_colors_init() {
+	if [ -t 1 ]; then
+		sf_start=${STARFIGHTER_ESC_START}
+		sf_end=${STARFIGHTER_ESC_END}
+		sf_bold=$(tput bold)
+		if [ -n "${sf_bold}" ]; then
+			sf_ncolors=$(tput colors)
+			if [ -n "${sf_ncolors}" ] && [ ${sf_ncolors} -ge 8 ]; then
+				STARFIGHTER_BLUE=${sf_start}${sf_bold}$(tput setaf 4)${sf_end}
+				STARFIGHTER_CYAN=${sf_start}${sf_bold}$(tput setaf 6)${sf_end}
+				STARFIGHTER_GRAY=${sf_start}${sf_bold}$(tput setaf 0)${sf_end}
+				STARFIGHTER_GREEN=${sf_start}${sf_bold}$(tput setaf 2)${sf_end}
+				STARFIGHTER_MAGENTA=${sf_start}${sf_bold}$(tput setaf 5)${sf_end}
+				STARFIGHTER_RED=${sf_start}${sf_bold}$(tput setaf 1)${sf_end}
+				STARFIGHTER_WHITE=${sf_start}${sf_bold}$(tput setaf 7)${sf_end}
+				STARFIGHTER_YELLOW=${sf_start}${sf_bold}$(tput setaf 3)${sf_end}
+				STARFIGHTER_NORMAL=${sf_start}$(tput sgr0)${sf_end}
+			else
+				STARFIGHTER_BLUE=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_CYAN=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_GRAY=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_GREEN=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_MAGENTA=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_RED=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_WHITE=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_YELLOW=${sf_start}${sf_bold}${sf_end}
+				STARFIGHTER_NORMAL=${sf_start}$(tput sgr0)${sf_end}
+			fi
+			unset sf_ncolors
 		fi
-		unset sf_ncolors
+		unset sf_start sf_end sf_bold
 	fi
-	unset sf_start sf_end sf_bold
-fi
+}
+
+starfighter_colors_init
 
 case " ${STARFIGHTER_DEBUG} " in
 *" colors "*)
