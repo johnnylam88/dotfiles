4ce7633ed9240f2c257ec5204a838876c11b7b12
Johnny C. Lam <jlam88@gmail.com>
2022-12-27 17:28:11 -0500

    fix: use a smaller arrow in `spaceship` theme
    Use a smaller arrow in the `spaceship` theme to more closely match
    the how `spaceship-prompt` looks.

--- .config/shell/steamship/theme.sh
+++ .config/shell/steamship/theme.sh
@@ -20,7 +20,8 @@ steamship_theme_bracket() {
 
 STEAMSHIP_THEMES="${STEAMSHIP_THEMES} spaceship"
 steamship_theme_spaceship() {
-	STEAMSHIP_CHARACTER_SYMBOL='➜'
+	STEAMSHIP_CHARACTER_SYMBOL='→'
+	STEAMSHIP_CHARACTER_SYMBOL_ROOT='⇒'
 	STEAMSHIP_DELIMITER_SHOW='false'
 }
 
