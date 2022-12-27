92e510902b17fa1bf8ee99ac10d60c12d0eb8ab7
Johnny C. Lam <jlam88@gmail.com>
2022-12-26 21:30:15 -0500

    feat: pair up `delimiter` and `character` sections
    Choose default symbols for the `delimiter` and `character` sections
    so that they line up and for a connected glyph. The entire glyph
    changes to green or red depending on the exit code of the previous
    user command.
    
    Also choose similar default symbols for the root user.

--- .config/shell/steamship/character.sh
+++ .config/shell/steamship/character.sh
@@ -3,33 +3,33 @@
 : ${STEAMSHIP_CHARACTER_SHOW:='true'}
 : ${STEAMSHIP_CHARACTER_PREFIX:=${STEAMSHIP_NEWLINE}}
 : ${STEAMSHIP_CHARACTER_SUFFIX:=' '}
-: ${STEAMSHIP_CHARACTER_SYMBOL:='$'}
-: ${STEAMSHIP_CHARACTER_SYMBOL_ROOT:='#'}
+: ${STEAMSHIP_CHARACTER_SYMBOL:='└'}
+: ${STEAMSHIP_CHARACTER_SYMBOL_ROOT:='╙'}
 : ${STEAMSHIP_CHARACTER_SYMBOL_SUCCESS:=${STEAMSHIP_CHARACTER_SYMBOL}}
 : ${STEAMSHIP_CHARACTER_SYMBOL_FAILURE:=${STEAMSHIP_CHARACTER_SYMBOL}}
-: ${STEAMSHIP_CHARACTER_COLOR_SUCCESS:='GREEN'}
-: ${STEAMSHIP_CHARACTER_COLOR_FAILURE:='RED'}
+: ${STEAMSHIP_CHARACTER_COLOR_SUCCESS:=${STEAMSHIP_COLOR_SUCCESS}}
+: ${STEAMSHIP_CHARACTER_COLOR_FAILURE:=${STEAMSHIP_COLOR_FAILURE}}
 
 steamship_character() {
 	ssc_char=
 	ssc_color=
+	ssc_colorvar=
 	if [ -z "${STEAMSHIP_RETVAL}" ]; then
 		ssc_colorvar='STEAMSHIP_WHITE'
-		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL:='$'}
+		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL}
 	elif [ "${STEAMSHIP_RETVAL}" = 0 ]; then
 		ssc_colorvar="STEAMSHIP_${STEAMSHIP_CHARACTER_COLOR_SUCCESS}"
-		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL_SUCCESS:='$'}
+		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL_SUCCESS}
 	else
 		ssc_colorvar="STEAMSHIP_${STEAMSHIP_CHARACTER_COLOR_FAILURE}"
-		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL_FAILURE:='$'}
+		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL_FAILURE}
 	fi
 	eval 'ssc_color=${'${ssc_colorvar}'}'
-	unset ssc_colorvar
 
 	if	[ -n "${STEAMSHIP_DEBUG}" ] ||
 		steamship_user_is_root
 	then
-		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL_ROOT:='#'}
+		ssc_char=${STEAMSHIP_CHARACTER_SYMBOL_ROOT}
 	fi
 	# ${ssc_char} is always set and non-null.
 
@@ -37,7 +37,6 @@ steamship_character() {
 	if [ -n "${ssc_char}" ]; then
 		ssc_status=${ssc_char}
 	fi
-
 	if [ -n "${ssc_status}" ]; then
 		# Reset color to *normal* for user text.
 		ssc_status="${ssc_color}${ssc_status}${STEAMSHIP_NORMAL}"
@@ -47,7 +46,7 @@ steamship_character() {
 		ssc_status="${ssc_status}${STEAMSHIP_CHARACTER_SUFFIX}"
 	fi
 	echo "${ssc_status}"
-	unset ssc_char ssc_status
+	unset ssc_char ssc_color ssc_colorvar ssc_status
 }
 
 steamship_character_prompt() {
--- .config/shell/steamship/delimiter.sh
+++ .config/shell/steamship/delimiter.sh
@@ -3,38 +3,60 @@
 : ${STEAMSHIP_DELIMITER_SHOW:='true'}
 : ${STEAMSHIP_DELIMITER_PREFIX:=${STEAMSHIP_NEWLINE}}
 : ${STEAMSHIP_DELIMITER_SUFFIX:=${STEAMSHIP_SUFFIX_DEFAULT}}
-: ${STEAMSHIP_DELIMITER_SYMBOL:='❯'}
-: ${STEAMSHIP_DELIMITER_COLOR:='YELLOW'}
-: ${STEAMSHIP_DELIMITER_COLOR_ROOT:='RED'}
+: ${STEAMSHIP_DELIMITER_SYMBOL:='┌'}
+: ${STEAMSHIP_DELIMITER_SYMBOL_ROOT:='╓'}
+: ${STEAMSHIP_DELIMITER_SYMBOL_SUCCESS:=${STEAMSHIP_DELIMITER_SYMBOL}}
+: ${STEAMSHIP_DELIMITER_SYMBOL_FAILURE:=${STEAMSHIP_DELIMITER_SYMBOL}}
+: ${STEAMSHIP_DELIMITER_COLOR_SUCCESS:=${STEAMSHIP_COLOR_SUCCESS}}
+: ${STEAMSHIP_DELIMITER_COLOR_FAILURE:=${STEAMSHIP_COLOR_FAILURE}}
 
 steamship_delimiter() {
+	ssd_char=
+	ssd_color=
+	ssd_colorvar=
+	if [ -z "${STEAMSHIP_RETVAL}" ]; then
+		ssd_colorvar='STEAMSHIP_WHITE'
+		ssd_char=${STEAMSHIP_DELIMITER_SYMBOL}
+	elif [ "${STEAMSHIP_RETVAL}" = 0 ]; then
+		ssd_colorvar="STEAMSHIP_${STEAMSHIP_DELIMITER_COLOR_SUCCESS}"
+		ssd_char=${STEAMSHIP_DELIMITER_SYMBOL_SUCCESS}
+	else
+		ssd_colorvar="STEAMSHIP_${STEAMSHIP_DELIMITER_COLOR_FAILURE}"
+		ssd_char=${STEAMSHIP_DELIMITER_SYMBOL_FAILURE}
+	fi
+	eval 'ssd_color=${'${ssd_colorvar}'}'
+
 	if	[ -n "${STEAMSHIP_DEBUG}" ] ||
 		steamship_user_is_root
 	then
-		ssd_colorvar="STEAMSHIP_${STEAMSHIP_DELIMITER_COLOR_ROOT}"
-	else
-		ssd_colorvar="STEAMSHIP_${STEAMSHIP_DELIMITER_COLOR}"
+		ssd_char=${STEAMSHIP_DELIMITER_SYMBOL_ROOT}
 	fi
-	eval 'ssd_color=${'${ssd_colorvar}'}'
-	unset ssd_colorvar
+	# ${ssd_char} is always set and non-null.
 
 	ssd_status=
-	if [ -n "${STEAMSHIP_DELIMITER_SYMBOL}" ]; then
-		ssd_status="${ssd_color}${STEAMSHIP_DELIMITER_SYMBOL}${STEAMSHIP_WHITE}"
+	if [ -n "${ssd_char}" ]; then
+		ssd_status=${ssd_char}
+	fi
+	if [ -n "${ssd_status}" ]; then
+		ssd_status="${ssd_color}${ssd_status}${STEAMSHIP_WHITE}"
 		if [ "${1}" = '-p' ]; then
 			ssd_status="${STEAMSHIP_DELIMITER_PREFIX}${ssd_status}"
 		fi
 		ssd_status="${ssd_status}${STEAMSHIP_DELIMITER_SUFFIX}"
 	fi
 	echo "${ssd_status}"
-	unset ssd_status ssd_color
+	unset ssd_char ssd_color ssd_colorvar ssd_status
 }
 
 steamship_delimiter_prompt() {
 	[ "${STEAMSHIP_DELIMITER_SHOW}" = true ] || return
 
 	# Prepend status to ${STEAMSHIP_PROMPT}.
-	STEAMSHIP_PROMPT="$(steamship_delimiter -p)${STEAMSHIP_PROMPT}"
+	if [ "${STEAMSHIP_PROMPT_HAS_COMMAND_SUBST}" = true ]; then
+		STEAMSHIP_PROMPT='$(steamship_delimiter -p)'"${STEAMSHIP_PROMPT}"
+	else
+		STEAMSHIP_PROMPT="$(steamship_delimiter -p)${STEAMSHIP_PROMPT}"
+	fi
 }
 
 case " ${STEAMSHIP_DEBUG} " in
