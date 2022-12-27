d09919ca6fe2ba1deb732f835dfaab564bd9fd07
Johnny C. Lam <jlam88@gmail.com>
2022-12-27 17:19:40 -0500

    fix: change default theme and add a `bracket` theme
    Change the default `steamship` theme back to using a right carat
    for the prompt delimiter and a `$` or `#` for the prompt character.
    This makes the theme look correct regardless of which sections are
    displayed.
    
    Add a `bracket` theme to have a connected prompt delimiter and
    character, and force hiding any sections that appear between
    `line_separator` and `character` in the prompt order so that the
    prompt delimiter and character and stay aligned.

--- .config/shell/steamship/theme.sh
+++ .config/shell/steamship/theme.sh
@@ -1,33 +1,43 @@
 # steamship/theme.sh
 
-STEAMSHIP_THEMES='default'
-steamship_theme_default() {
+STEAMSHIP_THEMES=
+
+STEAMSHIP_THEMES="${STEAMSHIP_THEMES} ascii"
+steamship_theme_ascii() {
+	STEAMSHIP_CHARACTER_SYMBOL='$'
+	STEAMSHIP_CHARACTER_SYMBOL_ROOT='#'
+	STEAMSHIP_DELIMITER_SHOW='false'
+}
+
+STEAMSHIP_THEMES="${STEAMSHIP_THEMES} bracket"
+steamship_theme_bracket() {
 	STEAMSHIP_CHARACTER_SYMBOL='└'
 	STEAMSHIP_CHARACTER_SYMBOL_ROOT='╙'
 	STEAMSHIP_DELIMITER_SYMBOL='┌'
 	STEAMSHIP_DELIMITER_SYMBOL_ROOT='╓'
-}
-
-STEAMSHIP_THEMES="${STEAMSHIP_THEMES} ascii"
-steamship_theme_ascii() {
-	STEAMSHIP_DELIMITER_SHOW='false'
-	STEAMSHIP_CHARACTER_SYMBOL='$'
-	STEAMSHIP_CHARACTER_SYMBOL_ROOT='#'
+	STEAMSHIP_EXIT_CODE_SHOW='false'
 }
 
 STEAMSHIP_THEMES="${STEAMSHIP_THEMES} spaceship"
 steamship_theme_spaceship() {
-	STEAMSHIP_DELIMITER_SHOW='false'
 	STEAMSHIP_CHARACTER_SYMBOL='➜'
+	STEAMSHIP_DELIMITER_SHOW='false'
+}
+
+STEAMSHIP_THEMES="${STEAMSHIP_THEMES} steamship"
+steamship_theme_steamship() {
+	STEAMSHIP_CHARACTER_SYMBOL='$'
+	STEAMSHIP_CHARACTER_SYMBOL_ROOT='#'
+	STEAMSHIP_DELIMITER_SYMBOL='❯'
 }
 
 steamship_theme() {
-	sst_theme=${1:-'default'}
+	sst_theme=${1:-'steamship'}
 	case " ${STEAMSHIP_THEMES} " in
 	*" ${sst_theme} "*)
 		;;
 	*)
-		echo 1>&2 "steamship_theme: \`${sst_theme}' theme not found, using default."
+		echo 1>&2 "steamship_theme: \`${sst_theme}' theme not found, using \`steamship'."
 		sst_theme='default'
 	esac
 	sst_theme_fn="steamship_theme_${sst_theme}"
