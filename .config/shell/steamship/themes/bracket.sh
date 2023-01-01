5cde06f2e65cff2ad5f554aaf348253c640bb11e
Johnny C. Lam <jlam88@gmail.com>
2022-12-31 23:45:16 -0500

    fix: set variables needed to make `bracket` theme display correctly
    The `bracket` theme always needs the `character`, `delimiter`, and
    `line_separator` sections to show, and always needs the `exit_code`
    section *NOT* to show, in order to properly align the left bracket.

--- .config/shell/steamship/themes/bracket.sh
+++ .config/shell/steamship/themes/bracket.sh
@@ -4,6 +4,9 @@
 STEAMSHIP_THEMES="${STEAMSHIP_THEMES} bracket"
 
 steamship_theme_bracket() {
+	# Delimiter and character symbols that form a left bracket
+	# between the two lines of the prompt.
+
 	STEAMSHIP_CHARACTER_SHOW='true'
 	STEAMSHIP_CHARACTER_SYMBOL='└ '
 	STEAMSHIP_CHARACTER_SYMBOL_ROOT='╙ '
@@ -16,7 +19,12 @@ steamship_theme_bracket() {
 	STEAMSHIP_DELIMITER_SYMBOL_SUCCESS=${STEAMSHIP_DELIMITER_SYMBOL}
 	STEAMSHIP_DELIMITER_SYMBOL_FAILURE=${STEAMSHIP_DELIMITER_SYMBOL}
 
-	# Always disable exit_code because it will break the alignment
-	# of the delimiter and character symbols.
+	# Always disable exit_code because it will break the alignment of
+	# the delimiter and character symbols.
 	STEAMSHIP_EXIT_CODE_SHOW='false'
+
+	# Always show the line separator within the prompt so that it will
+	# span two lines, which allows for the delimiter and character
+	# symbols to align.
+	STEAMSHIP_LINE_SEPARATOR_SHOW='true'
 }
