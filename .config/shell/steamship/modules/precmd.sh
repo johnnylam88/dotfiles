a9f8d08fc54547e7a3f334220c31e0ca5761d214
Johnny C. Lam <jlam88@gmail.com>
2023-01-02 18:14:35 -0500

    feat: generalize `precmd` to allow other modules to add hooks
    Maintain a list of hook functions to run at `precmd` time before the
    prompt is evaluated. Modules can add hook functions using
    `steamship_precmd_add_hook <function_name>`.

--- .config/shell/steamship/modules/precmd.sh
+++ .config/shell/steamship/modules/precmd.sh
@@ -4,14 +4,34 @@
 case " ${STEAMSHIP_MODULES_SOURCED} " in *" precmd "*) return ;; esac
 
 steamship_precmd_init() {
-	: "do nothing"
+	steamship_precmd_hooks=
+}
+
+steamship_precmd_run_hooks() {
+	if [ -n "${steamship_precmd_hooks}" ]; then
+		for ssprh_hook_fn in ${steamship_precmd_hooks}; do
+			eval "${ssprh_hook_fn}"
+		done
+		unset ssprh_hook_fn
+	fi
+}
+
+steamship_precmd_add_hook() {
+	sspah_hook_fn=${1}
+	if [ -n "${sspah_hook_fn}" ]; then
+		if [ -z "${steamship_precmd_hooks}" ]; then
+			steamship_precmd_hooks=${sspah_hook_fn}
+		else
+			steamship_precmd_hooks="${steamship_precmd_hooks} ${sspah_hook_fn}"
+		fi
+	fi
 }
 
 steamship_precmd_prompt() {
 	[ "${STEAMSHIP_PROMPT_COMMAND_SUBST}" = true ] || return
 
 	# shellcheck disable=SC2089,SC2016
-	sspp_prefix='$(STEAMSHIP_RETVAL=$? && echo "'
+	sspp_prefix='$(STEAMSHIP_RETVAL=$?; steamship_precmd_run_hooks; echo "'
 	sspp_suffix='")'
 	sspp_prompt="${sspp_prefix}${STEAMSHIP_PROMPT}${sspp_suffix}"
 	STEAMSHIP_PROMPT=${sspp_prompt}
