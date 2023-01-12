0e756ef506645888591b6ab2b0dc49105ab32cd9
Johnny C. Lam <jlam88@gmail.com>
2023-01-12 00:06:22 -0500

    feat: add a `utils` library based on spaceship `utils.zsh`
    Add a `utils.sh` library file that adds some helper functions to be used
    by modules. The functions are based on similarly-named functions in the
    spaceship `utils.zsh` library file.
    
    The `upsearch` function currently differs from the spaceship one in that
    it is a pure shell implementation instead of using `find`.

---
+++ .config/shell/steamship/lib/utils.sh
@@ -0,0 +1,79 @@
+# steamship/lib/utils.sh
+# shellcheck shell=sh
+
+case " ${STEAMSHIP_LIBS_SOURCED} " in *" utils "*) return ;; esac
+
+# Check if a command exists in ${PATH}.
+# USAGE:
+#     steamship_exists <command>
+steamship_exists() {
+	command -v "${1}" >/dev/null 2>&1
+}
+
+# Check if the current directory is in a Git repository.
+# USAGE:
+#     steamship_is_git
+steamship_is_git() {
+	steamship_exists git &&
+	[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = true ]
+}
+
+# Print the root of the Git repository.
+# USAGE:
+#     steamship_git_repo_path
+steamship_git_repo_path() {
+	steamship_exists git &&
+	git rev-parse --show-toplevel 2>/dev/null
+}
+
+# Save the parameter list as quoted strings separated by newlines.
+# USAGE:
+#     steamship_save_argv "$@"
+# EXAMPLE:
+#     argv=$(steamship_save_argv "#@")   # save parameters
+#     set -- foo bar baz boo             # set new parameters
+#     eval "set -- ${argv}"              # restore saved parameters
+steamship_save_argv() {
+	for sssa_arg; do
+		printf '%s\n' "${sssa_arg}" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/'/"
+	done
+	echo " "
+	unset sssa_arv
+}
+
+# Search upward through the directory tree from the current directory
+# for a specific file or directory. Returns the path to the first found
+# file or fails.
+# USAGE:
+#     steamship_upsearch <glob1> ...
+# EXAMPLE:
+#     $ steamship_upsearch package.json node_modules
+#     /home/username/path/to/project/node_modules
+steamship_upsearch() (
+	# Returns the path to the first file found in an "upsearch" from the
+	# current directory.
+	ssu_argv=$(steamship_save_argv "$@")
+	ssu_root=${PWD}
+	while [ -n "${ssu_root}" ]; do
+		for ssu_arg in ${ssu_argv}; do
+			eval "set -- ${ssu_arg}"
+			for ssu_glob; do
+				for ssu_name in ${ssu_glob}; do
+					ssu_path="${ssu_root}/${ssu_name}"
+					if [ -e "${ssu_path}" ]; then
+						echo "${ssu_path}"
+						return 0
+					fi
+				done
+			done
+		done
+
+		[ -d "${ssu_root}/.git" ] && return 1
+
+		ssu_root=${ssu_root%/*}
+		[ -z "${ssu_root}" ] || cd ..
+	done
+	return 1
+)
+
+STEAMSHIP_LIBS_SOURCED="${STEAMSHIP_LIBS_SOURCED} utils"
