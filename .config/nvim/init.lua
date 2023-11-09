6740b0da75763cad08004123a142c292404b92c2
Johnny C. Lam <jlam88@gmail.com>
2023-11-08 21:23:58 -0500

    fix: simplify code to bootstrap plugins for Neovim
    Refactor the bootstrap code into a separate function and use it to
    install the required plugins for the Neovim configuration.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,39 +4,31 @@
 -- Path to plugins managed by lazy.vim.
 local nvim_plugins_path = vim.fn.stdpath("data") .. "/lazy"
 
--- Bootstrap lazy.nvim plugin manager.
-local lazy_path = nvim_plugins_path .. "/lazy.nvim"
-if not vim.loop.fs_stat(lazy_path) then
-  vim.notify("Bootstrapping lazy.nvim...", vim.log.levels.INFO)
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazy_path,
-  })
+local function install_package(name, alias)
+  local _, _, owner, repo = name:find([[(.+)/(.+)]])
+  local path = ("%s/%s"):format(nvim_plugins_path, alias or repo)
+
+  if not vim.loop.fs_stat(path) then
+    vim.notify(("Installing %s/%s..."):format(owner, repo), vim.log.levels.INFO)
+
+    local command = {
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "--single-branch",
+      ("https://github.com/%s/%s.git"):format(owner, repo),
+      path,
+    }
+
+    vim.fn.system(command)
+  end
+
+  vim.opt.runtimepath:prepend(path)
 end
 
--- Bootstrap hotpot.nvim to allow using Fennel on top of Lua.
-local hotpot_path = nvim_plugins_path .. "/hotpot.nvim"
-if not vim.loop.fs_stat(hotpot_path) then
-  vim.notify("Bootstrapping hotpot.nvim...", vim.log.levels.INFO)
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/rktjmp/hotpot.nvim.git",
-    hotpot_path,
-  })
-end
-
--- Add lazy.nvim and hotpot.nvim paths to runtimepath.
-vim.opt.runtimepath:prepend({
-  hotpot_path,
-  lazy_path
-})
+-- Bootstrap required plugins.
+install_package("folke/lazy.nvim") -- plugin manager
+install_package("rktjmp/hotpot.nvim") -- Fennel for neovim
 
 -- Configure hotpot.nvim.
 require("hotpot").setup({
@@ -60,7 +52,7 @@ if vim.loop.fs_stat(config_plugins_path) then
   --   https://github.com/folke/lazy.nvim#-plugin-spec
   for file in vim.fs.dir(config_plugins_path) do
     file = file:match("^(.*)%.fnl$")
-    plugins[#plugins + 1] = require("config.plugins." .. file)
+    table.insert(plugins, require("config.plugins." .. file))
   end
 end
 
